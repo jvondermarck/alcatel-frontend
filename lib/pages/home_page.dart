525e14c07b69feb220662df51db4486249512429
bastianfbr <badmaxyt@gmail.com>
2023-09-22 23:19:02 +0200

    feat: :lipstick: update background and clean widget tree

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,21 +4,18 @@ class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<HomePage> createState() => _MyHomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _MyHomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
+      backgroundColor: Color.fromRGBO(97, 67, 209, 1),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'This is a text widget',
-            ),
-          ],
+          children: <Widget>[],
         ),
       ),
     );
