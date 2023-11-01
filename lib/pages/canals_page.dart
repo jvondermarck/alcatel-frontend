f2c3b374bb8e80d1eba48c5680da1766092a7a2a
bastianfbr <badmaxyt@gmail.com>
2023-11-01 18:20:05 +0100

    feat: :lipstick: update Tile to card and white background

--- lib/pages/canals_page.dart
+++ lib/pages/canals_page.dart
@@ -11,16 +11,18 @@ class CanalsPage extends StatefulWidget {
 class _CanalsPageState extends State<CanalsPage> {
   @override
   Widget build(BuildContext context) {
-    Widget canalTile(String title, String subtitle) {
+    Widget canalCard(String title, String subtitle) {
       return Padding(
         padding: const EdgeInsets.only(top: 8.0),
-        child: ListTile(
-          title: Text(title),
-          subtitle: Text(subtitle),
-          leading: const Icon(Icons.person),
-          iconColor: Colors.white,
-          textColor: Colors.white,
-          trailing: const Icon(Icons.meeting_room_rounded),
+        child: Card(
+          child: ListTile(
+            title: Text(title),
+            subtitle: Text(subtitle),
+            leading: const Icon(Icons.person),
+            iconColor: Colors.black,
+            textColor: Colors.black,
+            trailing: const Icon(Icons.meeting_room_rounded),
+          ),
         ),
       );
     }
@@ -43,11 +45,14 @@ class _CanalsPageState extends State<CanalsPage> {
             foregroundColor: Colors.white,
             centerTitle: true,
           ),
-          body: ListView.builder(
-              itemCount: 5,
-              itemBuilder: (context, index) {
-                return canalTile('Canal $index', 'Description $index');
-              }),
+          body: Container(
+            color: Colors.white,
+            child: ListView.builder(
+                itemCount: 5,
+                itemBuilder: (context, index) {
+                  return canalCard('Canal $index', 'Description $index');
+                }),
+          ),
         ));
   }
 }
