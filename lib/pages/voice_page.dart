0bd81d635024bae25e5a1f82971bb81e715cae04
bastianfbr <badmaxyt@gmail.com>
2023-11-02 14:33:41 +0100

    feat: :lipstick: add VoicePage to control PTT

---
+++ lib/pages/voice_page.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:pushtotalk/components/base_scaffold.dart';
+import 'package:pushtotalk/utils/colors.dart';
+
+class VoicePage extends StatefulWidget {
+  final String canal;
+
+  const VoicePage({super.key, required this.canal});
+
+  @override
+  State<VoicePage> createState() => _VoicePageState();
+}
+
+class _VoicePageState extends State<VoicePage> {
+  bool isPressed = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return BaseScaffold(
+        body: Container(
+          color: Colors.white,
+          child: Center(
+              child: ButtonBar(
+            alignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    isPressed = !isPressed;
+                  });
+                },
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(300, 300),
+                  shape: const CircleBorder(),
+                  backgroundColor: isPressed ? primaryColor : Colors.white,
+                ),
+                child: Icon(
+                  isPressed ? Icons.mic : Icons.mic_off,
+                  size: 200,
+                  color: isPressed ? Colors.white : primaryColorLight,
+                ),
+              )
+            ],
+          )),
+        ),
+        title: widget.canal);
+  }
+}
