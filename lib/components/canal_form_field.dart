f3940d5d816325a8649979476813139cb9bbc788
bastianfbr <badmaxyt@gmail.com>
2023-11-02 10:55:31 +0100

    feat: :sparkles: add controller in component

--- lib/components/canal_form_field.dart
+++ lib/components/canal_form_field.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 
 class CanalFormField extends StatefulWidget {
-  const CanalFormField({
-    super.key,
-    required this.title,
-    required this.hint,
-  });
+  const CanalFormField(
+      {super.key,
+      required this.title,
+      required this.hint,
+      required this.controller});
 
   final String title;
   final String hint;
+  final TextEditingController controller;
 
   @override
   State<CanalFormField> createState() => _CanalFormFieldState();
@@ -18,6 +19,7 @@ class _CanalFormFieldState extends State<CanalFormField> {
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      controller: widget.controller,
       decoration: InputDecoration(
           labelText: widget.title,
           floatingLabelStyle: const TextStyle(fontSize: 19),
