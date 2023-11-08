8c695d5794dab0f0a4e45ab56f118f51284147ea
bastianfbr <badmaxyt@gmail.com>
2023-11-09 00:13:34 +0100

    feat: :sparkles: add logLevel and enhance function

--- android/app/src/main/kotlin/com/ale/pushtotalk/App.kt
+++ android/app/src/main/kotlin/com/ale/pushtotalk/App.kt
@@ -1,8 +1,11 @@
 package com.ale.pushtotalk
 
-import io.flutter.app.FlutterApplication
-import com.ale.rainbowsdk.RainbowSdk
 import android.util.Log
+import com.ale.rainbow.RBLog
+import com.ale.rainbowsdk.RainbowSdk
+import io.flutter.app.FlutterApplication
+import org.tinylog.Level
+
 
 class App : FlutterApplication() {
     private val applicationID = "6ec7a6107e7711eeb0c4e7a30078a6db"
@@ -12,17 +15,14 @@ class App : FlutterApplication() {
     override fun onCreate() {
         super.onCreate()
         RainbowSdk.instance().initialize(this, applicationID, applicationSecret);
+        RBLog.setLevel(Level.WARN);
+        Log.d("RainbowSdk", "onCreate: ${RainbowSdk.instance().isInitialized}");
         RainbowSdk.instance().push().activate(this);
     }
-
     companion object {
-        private var rainbowSdkInstance: RainbowSdk? = null
 
         fun getRainbowSdkInstance(): RainbowSdk {
-            if (rainbowSdkInstance == null) {
-                rainbowSdkInstance = RainbowSdk.instance()
-            }
-            return rainbowSdkInstance!!
+            return RainbowSdk.instance();
         }
     }
 
