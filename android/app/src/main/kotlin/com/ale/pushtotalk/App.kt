6f072ed281bd422612ade36042478d9639751707
bastianfbr <badmaxyt@gmail.com>
2023-11-15 12:13:01 +0100

    feat: :sparkles: add dotenv configuration for RainbowSdk

--- android/app/src/main/kotlin/com/ale/pushtotalk/App.kt
+++ android/app/src/main/kotlin/com/ale/pushtotalk/App.kt
@@ -2,15 +2,20 @@ package com.ale.pushtotalk
 
 import com.ale.pushtotalk.interfaces.RainbowService
 import com.ale.pushtotalk.services.RainbowServiceImpl
+import io.github.cdimascio.dotenv.dotenv
 import io.flutter.app.FlutterApplication
 
 
 class App(private val rainbowService: RainbowService = RainbowServiceImpl()) : FlutterApplication() {
 
+    private val dotenv = dotenv {
+        directory = "/assets"
+        filename = "env" // instead of '.env', use 'env'
+    }
+
     // TODO - Hide ID/Secret (config file)
-    private val applicationID = "c40a10407e6311eeb0c4e7a30078a6db"
-    private val applicationSecret =
-        "NTo25SAvcOKf00TnamieEVWuei5V1HiS9dCtr8sfXPil3MasyQB0B6Q5wLQ9uNOP"
+    private val applicationID = dotenv["APPLICATION_ID"]
+    private val applicationSecret = dotenv["APPLICATION_SECRET"]
 
     override fun onCreate() {
         super.onCreate()
