3542be29247f31284b48b3451aa031928a178e8e
max1lock <alex.gerberon@gmail.com>
2023-11-30 18:19:16 +0100

    #40 Ask for GPS Permissions & print GPS position

--- lib/pages/bubbles_page.dart
+++ lib/pages/bubbles_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:pushtotalk/components/base_scaffold.dart';
 import 'package:pushtotalk/components/bubble_card.dart';
 import 'package:pushtotalk/components/bubble_creation_form.dart';
+import 'package:geolocator/geolocator.dart';
 
 class BubblesPage extends StatefulWidget {
   const BubblesPage({Key? key}) : super(key: key);
@@ -11,14 +12,62 @@ class BubblesPage extends StatefulWidget {
 }
 
 class _BubblesPageState extends State<BubblesPage> {
-  List<BubbleCard> bubbleList = [];
+  @override
+  void initState() {
+    // print position
+    _determinePosition().then((value) => print(value));
+    super.initState();
+  }
 
+  /// Determine the current position of the device.
+  ///
+  /// When the location services are not enabled or permissions
+  /// are denied the `Future` will return an error.
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition();
+  }
+
+  List<BubbleCard> bubbleList = [];
   @override
   Widget build(BuildContext context) {
     return BaseScaffold(
       title: 'Liste des canaux',
       body: RefreshIndicator(
         onRefresh: () async {
+          _determinePosition().then((value) => print(value));
           await Future.delayed(const Duration(seconds: 2));
         },
         child: Container(
